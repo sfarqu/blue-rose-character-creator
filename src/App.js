f70566e51d23847744a5ff4b8890666aca35f402
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2020-08-02 17:52:04 -0700

    Initial character record implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,78 @@ import './App.css';
 import RollSet from './RollSet';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+let character = {
+  name: null,
+  attributes: [
+    {
+      name: "accuracy",
+      value: null
+    },
+    {
+      name: "communication",
+      value: null
+    },
+    {
+      name: "constitution",
+      value: null
+    },
+    {
+      name: "dexterity",
+      value: null
+    },
+    {
+      name: "fighting",
+      value: null
+    },
+    {
+      name: "intelligence",
+      value: null
+    },
+    {
+      name: "perception",
+      value: null
+    },
+    {
+      name: "strength",
+      value: null
+    },
+    {
+      name: "willpower",
+      value: null
+    },
+  ],
+  race: null,
+  background: null,
+  class: null,
+  focuses: {},
+  arcana: {},
+  talents: {},
+  weapons: {},
+  relationships: [{
+    name: null,
+    intensity: null,
+    bond: null
+  }],
+  powers: {},
+  specializations: {},
+  speed: {},
+  defense: null,
+  armor: {},
+  health: null,
+  fatigue: null,
+  level: null,
+  persona: {
+    calling: null,
+    destiny: null,
+    fate: null,
+    corruption: null,
+    goals: {}
+  },
+  gear: {},
+  items: {},
+  titles: {}
+};
+
 const PrimaryLayout = () => (
   <div className="App">
     <header className="App-header">
