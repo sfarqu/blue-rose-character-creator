b21ac8f9be2dd60fc44c8cf0b81fe80e4278cad9
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2020-08-03 22:39:43 -0700

    More tests for alternate modes in Attribute

--- src/Attribute.test.js
+++ src/Attribute.test.js
@@ -8,3 +8,25 @@ test('Attribute name and value rendered', () => {
   expect(screen.getByText('Swords')).toBeInTheDocument();
   expect(screen.getByText('2')).toBeInTheDocument();
 });
+
+test('Non-adjustable attribute does not include buttons', () => {
+  render(<Attribute name="Swords" value={2} />);
+  expect(screen.queryByText('+')).not.toBeInTheDocument();
+})
+
+test('Non-adjustable attribute updates with props', () => {
+  const { getByTestId, rerender } = render(<Attribute name="Swords" value={2} />);
+  rerender(<Attribute name="Swords" value={4} />)
+  expect(screen.getByText('4')).toBeInTheDocument();
+})
+
+test('Adjustable attribute includes buttons', () => {
+  render(<Attribute name="Swords" value={2} adjustable={true} />);
+  expect(screen.getByText('+')).toBeInTheDocument();
+})
+
+test('Adjustable attribute does not update with props', () => {
+  const { getByTestId, rerender } = render(<Attribute name="Swords" value={2} adjustable={true} />);
+  rerender(<Attribute name="Swords" value={4} adjustable={true} />)
+  expect(screen.getByText('2')).toBeInTheDocument();
+})
