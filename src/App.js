5b57565b0ac046e7d44f8d80752953ee3ca5c75b
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2020-08-06 16:48:37 -0700

    Add relationships page

--- src/App.js
+++ src/App.js
@@ -172,6 +172,9 @@ class PrimaryLayout extends Component {
         <Route path="/calling" exact render={(routeProps) => (
           <CallingPage {...routeProps} {...this.props} />
         )} />
+        <Route path="/relationships" exact render={(routeProps) => (
+          <RelationshipsPage {...routeProps} {...this.props} />
+        )} />
     </div>
     )
   }
@@ -589,6 +592,31 @@ const CallingPage = (props) => {
           <RoyalRoad trait="calling" />
           <RoyalRoad trait="destiny" />
           <RoyalRoad trait="fate" />
+        </div>
+        <div className="Description"></div>
+      </main>
+    </div>
+  );
+}
+
+
+const RelationshipsPage = (props) => {
+  const relationships = props.relationships ? props.relationships : [{name: "", intensity: 2, bond: ""},{name: "", intensity: 1, bond: ""}]
+  const relationshipObjects = relationships.map(value => (
+    <div className="module">
+      <input type="text" value={value.name} /><Attribute value={value.intensity} min={0} max={2} adjustable={true} /><input type="text" value={value.bond} />
+    </div>
+  ))
+  return (
+    <div>
+      <header className="App-page-header">
+        <Navigation target="/calling" direction="left" text="Back" />
+        <h1>Goals & Relationships</h1>
+        <Navigation target="/character" direction="right" text="Next" />
+      </header>
+      <main className="App-body">
+        <div className="Special">
+          {relationshipObjects}
         </div>
         <div className="Description"></div>
       </main>
