4f4b95c61411d0d21ef9b2511ee7f68ceaf37d3b
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2020-08-31 01:26:07 -0700

    Fix navigation from attributes

--- src/views/AttributesPage.js
+++ src/views/AttributesPage.js
@@ -23,12 +23,13 @@ class AttributesPage extends Component {
 
   render() {
     const attrs = this.props.attributes.map(value => <Attribute name={value.name} value={value.value} max={3} min={0} />);
+    const race = this.props.race.name ? this.props.race.name : ""
     return (
       <div>
         <header className="App-page-header">
           <Navigation target="/" direction="left" text="Back" />
           <h1>Roll for Attributes</h1>
-          <Navigation target="/race" direction="right" text="Next" />
+          <Navigation target={"/race/" + race} direction="right" text="Next" />
         </header>
         <main className="App-body">
             <RollSet numberOfRolls={9} numberOfDice={3} callback={(vals) => this.updateAttributes(vals)} />
@@ -47,6 +48,7 @@ class AttributesPage extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    race: state.raceBonuses,
     attributes: state.attributes
   }
 }
